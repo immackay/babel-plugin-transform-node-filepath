575b90c1c7067c11cb5a558c3df82efd45afe02b
Ian MacKay <immackay0@gmail.com>
2020-07-06 05:16:48 -0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function ({ types: t }) {
+  return {
+    name: "transform-node-filepath",
+    visitor: {
+      MemberExpression(path) {
+        if (path.matchesPattern("__filename")) {
+          path.replaceWith(t.valueToNode(__filename));
+        }
+        if (path.matchesPattern("__dirname")) {
+          path.replaceWith(t.valueToNode(__dirname));
+        }
+      },
+    },
+  };
+};
